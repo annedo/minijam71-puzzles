8c1615d094e1ab8992d02fc4fd8cf9d69f8de96b
nlaneau <31058891+nlaneau@users.noreply.github.com>
2021-01-09 23:40:37 -0500

    Created the layout of where game mechanics are. Implemented selection and adjacent check logic

---
+++ Assets/Scripts/IconSelection.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IconSelection : MonoBehaviour
+{
+    public bool Selected = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if Selected is true or false. Set sprite accordingly
+        // TODO - swap out sprite for highlighted sprite
+        // https://answers.unity.com/questions/741893/how-to-change-sprite-image-on-click.html
+
+        if (Input.GetMouseButtonDown(0))
+        {            
+            Selected = true;
+            var gm = gameObject.GetComponent<GridManager>();
+            gm.CheckSwap();
+        }
+    }
+}
