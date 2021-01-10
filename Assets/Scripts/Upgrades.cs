1a4c0408d741010e5053e5eb7daa9f20bc490425
nlaneau <31058891+nlaneau@users.noreply.github.com>
2021-01-10 17:48:39 -0500

    Added initial stuff for upgrade scene

---
+++ Assets/Scripts/Upgrades.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Upgrades : MonoBehaviour
+{
+    public Sprite spriteTier1;
+    public Sprite spriteTier2;
+    public Sprite spriteTier3;
+
+    public int CurrentTier = 1;
+
+    private SpriteRenderer _spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _spriteRenderer.sprite = spriteTier1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (CurrentTier)
+        {
+            case 1:
+                _spriteRenderer.sprite = spriteTier1;
+                break;
+            case 2:
+                _spriteRenderer.sprite = spriteTier2;
+                break;
+            case 3:
+                _spriteRenderer.sprite = spriteTier3;
+                break;
+        }
+    }
+
+    public void OnMouseDown()
+    {
+        // Money check
+        if (CurrentTier < 3)
+            CurrentTier++;
+        // Perform upgrade if money check success
+    }
+}
